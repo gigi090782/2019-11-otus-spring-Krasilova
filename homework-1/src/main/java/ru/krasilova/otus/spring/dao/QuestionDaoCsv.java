13c6f246d1404be0ce23a94be31fa9a20b46988c
Krasilova Marina <filatovamarina@mail.ru>
2019-12-07 12:59:14 +0300

    Добавлен класс исключения: NotCorrectFileWithQuestionsException. Название файла с вопросами передается параметром.

--- homework-1/src/main/java/ru/krasilova/otus/spring/dao/QuestionDaoCsv.java
+++ homework-1/src/main/java/ru/krasilova/otus/spring/dao/QuestionDaoCsv.java
@@ -1,6 +1,7 @@
 package ru.krasilova.otus.spring.dao;
 
 import ru.krasilova.otus.spring.domain.Question;
+import ru.krasilova.otus.spring.exceptions.NotCorrectFileWithQuestionsException;
 
 
 import java.io.InputStream;
@@ -11,15 +12,15 @@ import java.util.Scanner;
 public class QuestionDaoCsv implements QuestionDao {
 
     @Override
-    public List<Question> getQuestions() throws Exception {
+    public List<Question> getQuestions(String fileName) throws Exception {
         List<Question> questions = new ArrayList<>();
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("questions.csv");
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(fileName);
         Scanner scanner = new Scanner(inputStream);
         while (scanner.hasNext()) {
 
             String[] line = scanner.nextLine().split(";");
             if (line.length != 2)
-                throw new Exception("Некорректные данные");
+                throw new NotCorrectFileWithQuestionsException("Некорректные данные");
             questions.add(new Question(line[0], line[1]));
 
         }
---
+++ homework-1/src/main/java/ru/krasilova/otus/spring/exceptions/NotCorrectFileWithQuestionsException.java
@@ -0,0 +1,9 @@
+package ru.krasilova.otus.spring.exceptions;
+
+public class NotCorrectFileWithQuestionsException extends Exception {
+    public NotCorrectFileWithQuestionsException(String message) {
+        super(message);
+    }
+}
+
+
