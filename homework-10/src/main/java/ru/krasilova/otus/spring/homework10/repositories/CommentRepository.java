7324c4029c6ff2d9f53c74a93f1a59c504943dd4
Krasilova Marina <filatovamarina@mail.ru>
2020-02-14 20:17:10 +0300

    Домашнее задание №10 (Переписать приложение с использованием AJAX и REST-контроллеров)

---
+++ homework-10/src/main/java/ru/krasilova/otus/spring/homework10/repositories/CommentRepository.java
@@ -0,0 +1,14 @@
+package ru.krasilova.otus.spring.homework10.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.krasilova.otus.spring.homework10.models.Comment;
+
+import javax.transaction.Transactional;
+import java.util.List;
+@Transactional
+public interface CommentRepository  extends JpaRepository<Comment, Long>
+{
+    List<Comment> findByBookId(Long id);
+    List<Comment> findAll();
+    void deleteAllByBookId(long bookId);
+}
