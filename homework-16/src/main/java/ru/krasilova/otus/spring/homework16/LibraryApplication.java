39030d679fb6912de6a9a3cf19eaaa9d45f92efb
Krasilova Marina <filatovamarina@mail.ru>
2020-04-02 11:06:54 +0300

    Домашнее задание №16

---
+++ homework-16/src/main/java/ru/krasilova/otus/spring/homework16/LibraryApplication.java
@@ -0,0 +1,17 @@
+package ru.krasilova.otus.spring.homework16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+
+public class LibraryApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(LibraryApplication.class);
+
+    }
+
+
+}
